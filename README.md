ab0e99d58faee3997b44c2f231de433349e691f7
al-moreno <77991840+al-moreno@users.noreply.github.com>
2021-03-13 13:09:54 -0600

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# quiz
-fun quiz
+# Quiz Project
+
+## Descriptiion
+
+The short quiz is intended to test your knowledge on Looney Toons Cartoons.  It is just for fun so that I may try out some of the JavaScript tough during class and includes attempts at: declaration of global variables, object variables with several parameters for thequestions, functions with if else. I am also inclued variables with HTML block. functions for the time (which I don't believe I understand very well) and for loops. Enjoy. 
+
+## Installation
+
+No installation is needed other than prefered browers and a keyboard to enter criteria.
+
+- Open the browser to the following link:     https://al-moreno.github.io/Password-Homework/
+- Click the start button and begin the test.
+- At the end of the text you will be provided with your score.
+
+
+## Usage
+
+The coding on this application would be helpful (once all issue are fixed) with testing for verification that students understand material tough or to test job skills.
+
+![picture](screenshot.png)
+
+You may open my code for review with VSCode by accessing the following links.
+
+https://al-moreno......
+
+https://al-moreno......
+
+## This repo containes:
+- A ReadMe.md file
+- Index.html file
+- style.css file
+- script.js file
+- images 
+
+
+
+## Credits
+
+The follwoig idividuals and / or sites contributed and/or collaborated with the code:
+
+- for the HTML block in the JavaScript file:    https://www.tutorialrepublic.com/faq/how-to-assign-block-of-html-code-to-a-javascript-variable.php
+
+- for the idea of the CSS and some of the time: https://www.youtube.com/watch?v=pQr4O1OITJo
+
+- and the rest was thanks to my amazing instructor Rachel. 
+
+
+## License
+Uncertain of required licence so to CMA I am leaving the one pertaining to Trilogy Education Services.
+
+---
+
+© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
---
+++ script.js
@@ -0,0 +1,129 @@
+//Global lobal variables
+var currentQuestion = 0;
+var timeClock = 0;
+var timeLeft = 0;
+
+//arrray of the quiz questions, avaialble choices, and correct answers     
+var questions = [{title: "What color was Tweety?",
+    choices: ["black", "orange", "yellow"],
+    answer: "yellow"},
+
+    {title: "What was the name of the character that chased the Road Runner?",
+    choices: ["Daffy Duck", "Sylvester", "Wile E Cayote"],
+    answer: "Wile E Cayote "},
+
+    {title: " Who was the first ever Looney Too?",
+    choices: ["Elmer Fudd", "Sylvester", "Bosko", "None of the above."],
+    answer: "Bosko"},
+
+    {title: "In what year was Bugs Bunny born?",
+    choices: ["1940", "1941", "1937" ],
+    answer: "1940"},]; 
+
+
+//function to start the timer
+function start() {
+    timeLeft = 50;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+    
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+        endGame(); 
+    }
+}, 1000);
+
+    next();
+};
+
+//Stops timer
+function endGame() {
+    clearInterval(timer);
+
+var quizContent =   `<h3>Game Over</h3>
+                    <p>Your score was: ` + timeClock +  ` % out of 100, read up more </br>about Looney Toons and come back to try again. </p>
+                    <input id="initials" placeholder="Initials"></br>
+                    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+};
+
+//Local storage score
+function setScore() {
+    localStorage.setItem("topScore", timeClock);
+    localStorage.setItem("topScoreInt",  document.getElementById('initials').value);
+    
+    getScore();
+};
+
+function getScore() {
+var quizContent = `<h2>` + localStorage.getItem("topScoreInt") + `'s topScore is:</h2>
+                    <h1>` + localStorage.getItem("topScore") + `</h1><br> 
+                    <button onclick="clearScore()">Clear</button></br><button onclick="tryAgain()">Try Again</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+};
+
+
+//Clear score from localStorage
+function clearScore() {
+    localStorage.setItem("topScore", "");
+    localStorage.setItem("topScoreInt",  "");
+
+    tryAgain();
+};
+
+//reset the game 
+function tryAgain() {
+    clearInterval(timer);
+    
+document.getElementById("timeLeft").innerHTML = timeLeft;
+
+var quizContent =   `<h2> Looney Toon's</h2>
+                    <h3>How much do you know?</h3>
+                    <button onclick="start()"> Click to Start</button>`;
+
+document.getElementById("quizBody").innerHTML = quizContent;
+};
+
+//Score deduction for incorrect
+function incorrect() {
+    timeLeft -= 10; 
+    next();
+};
+
+//Score increased for correct
+function correct() {
+    timeClock += 10;
+    next();
+};
+
+//Questions
+function next() { 
+    currentQuestion++;
+
+if (currentQuestion > questions.length - 1) {
+    endGame();
+    return;
+};
+
+var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+    var buttonCode = "<button onclick=\"[selection]\">[option]</button>"; 
+    buttonCode = buttonCode.replace("[option]", questions[currentQuestion].choices[buttonLoop]);
+
+    if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+        buttonCode = buttonCode.replace("[selection]", "correct()");
+    } else {
+        buttonCode = buttonCode.replace("[selection]", "incorrect()");
+    }
+    quizContent += buttonCode
+}
+
+
+document.getElementById("quizBody").innerHTML = quizContent;
+};
